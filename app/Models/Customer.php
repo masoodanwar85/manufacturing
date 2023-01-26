cb3220633bc359ed146bb05347a2b403956090d1
Masood Anwar <masoodanwar85@gmail.com>
2023-01-26 19:33:18 +0500

    Updated

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -93,24 +93,24 @@ class Customer extends Model
 	public static function getMonthlyDefaulters(){
         DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
 	    $rawSQL = "
-                select
+                SELECT
                     customer.customerName,
                     tdOuter.subHeadID,
-                    SUM(case when tdOuter.isDebit = 0 then CAST(tdOuter.amount as SIGNED) * -1
-                    else tdOuter.amount end) as transactionAmount,
+                    SUM(CASE WHEN tdOuter.isDebit = 0 THEN CAST(tdOuter.amount AS SIGNED) * -1
+                    ELSE tdOuter.amount END) AS transactionAmount,
                     (
-                        select transaction.dateCreated 
-                        from transactiondetail
-                        inner join transaction on transaction.transactionID = transactionDetail.transactionID
-                        where headID in (" . \Config::get('constants.account_heads.customer_receivable') . ") and isDebit = 0 and subHeadID = tdOuter.subHeadID
-                        order by transaction.dateCreated DESC
-                        limit 0,1
-                    ) as lastReceivingDate
-                from customer
-                inner join transactionDetail tdOuter on tdOuter.subHeadID = customer.headID and tdOuter.headID in (" . \Config::get('constants.account_heads.customer_receivable') . "," . \Config::get('constants.account_heads.customer_payable') . ")
-                inner join transaction on transaction.transactionID = tdOuter.transactionID 
-                group by customer.customerName,tdOuter.subHeadID
-                having transactionAmount > 0 and (DATE_FORMAT(lastReceivingDate,'%Y-%m-01') < DATE_FORMAT(NOW(),'%Y-%m-01')) 
+                        SELECT transaction.dateCreated 
+                        FROM transactionDetail
+                        INNER JOIN transaction ON transaction.transactionID = transactionDetail.transactionID
+                        WHERE headID IN (" . \Config::get('constants.account_heads.customer_receivable') . ") AND isDebit = 0 AND subHeadID = tdOuter.subHeadID
+                        ORDER BY transaction.dateCreated DESC
+                        LIMIT 0,1
+                    ) AS lastReceivingDate
+                FROM customer
+                INNER JOIN transactionDetail tdOuter ON tdOuter.subHeadID = customer.headID AND tdOuter.headID IN (" . \Config::get('constants.account_heads.customer_receivable') . "," . \Config::get('constants.account_heads.customer_payable') . ")
+                INNER JOIN transaction ON transaction.transactionID = tdOuter.transactionID 
+                GROUP BY customer.customerName,tdOuter.subHeadID
+                HAVING transactionAmount > 0 AND ((DATE_FORMAT(lastReceivingDate,'%Y-%m-01') < DATE_FORMAT(NOW(),'%Y-%m-01')) OR lastReceivingDate IS NULL)
 	    ";
         return DB::select($rawSQL);
     }
