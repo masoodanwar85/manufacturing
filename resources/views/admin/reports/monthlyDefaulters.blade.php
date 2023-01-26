391427cc0b931c3cb0568b71bec9b617164c8fc2
Masood Anwar <masoodanwar85@gmail.com>
2023-01-26 19:39:23 +0500

    Updated

--- resources/views/admin/reports/monthlyDefaulters.blade.php
+++ resources/views/admin/reports/monthlyDefaulters.blade.php
@@ -25,7 +25,7 @@
                     <tr>
                         <td>{{$d->customerName}}</td>
                         <td>@money('$d->transactionAmount')</td>
-                        <td>{{date('d-m-Y', strtotime($d->lastReceivingDate))}}</td>
+                        <td>{{ date('F d, Y', strtotime($d->lastReceivingDate)) }}</td>
                     </tr>
                 @endforeach
                 </tbody>
