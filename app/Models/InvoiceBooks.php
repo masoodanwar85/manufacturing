ff3748a09883ae623be5cb9a30747476063d6bac
Masood Anwar <masood@octadyne.com>
2024-07-19 05:53:10 +0500

    Updated

--- app/Models/InvoiceBooks.php
+++ app/Models/InvoiceBooks.php
@@ -84,7 +84,7 @@ class InvoiceBooks extends Model
                 FROM serialNumber
                 LEFT JOIN invoiceBooks ON (invoiceBooks.startPage = serialNumber.id OR invoiceBooks.startpage <= serialNumber.id) AND (invoiceBooks.endPage = serialNumber.id OR invoiceBooks.endPage >= serialNumber.id)
                 LEFT JOIN bookSerials ON bookSerials.invoiceBookID = invoiceBooks.invoiceBookID AND bookSerials.serialNumber = serialNumber.id
-                LEFT JOIN `transaction` ON CONCAT_WS('-',SUBSTRING_INDEX(transactionTypeNumber,'-',2),TRIM(leading '0' from SUBSTRING_INDEX(transactionTypeNumber,'-',-1))) = CONCAT_WS('-', invoiceBooks.bookType, invoiceBooks.bookNumber, serialNumber.id)
+                LEFT JOIN `transaction` ON CONCAT_WS('-',SUBSTRING_INDEX(transactionTypeNumber,'-',2),TRIM(leading '0' from SUBSTRING_INDEX(transactionTypeNumber,'-',-1))) = CONCAT_WS('-', invoiceBooks.bookType, invoiceBooks.bookNumber, serialNumber.id) AND `transaction`.transactionTypeNumber LIKE '" . $params['bookType'] . "-%'
                 WHERE invoiceBooks.bookNumber IS NOT NULL AND `transaction`.transactionID IS NULL AND bookSerials.bookSerialID IS NULL
             ";
         }
