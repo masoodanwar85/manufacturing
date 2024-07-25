a658992ddaa371ca4649fbc872e51730452dcbec
Masood Anwar <masood@octadyne.com>
2024-07-25 06:00:30 +0500

    Updated

--- app/Models/InvoiceBooks.php
+++ app/Models/InvoiceBooks.php
@@ -82,7 +82,8 @@ class InvoiceBooks extends Model
             $rawSQL = "
                 SELECT serialNumber.id as sequenceNumber, CONCAT_WS('-',invoiceBooks.bookType,invoiceBooks.bookNumber,serialNumber.id) as serial,bookSerials.bookSerialID,invoiceBooks.*
                 FROM serialNumber
-                LEFT JOIN invoiceBooks ON (invoiceBooks.startPage = serialNumber.id OR invoiceBooks.startpage <= serialNumber.id) AND (invoiceBooks.endPage = serialNumber.id OR invoiceBooks.endPage >= serialNumber.id)
+                INNER JOIN invoiceBooks ON invoiceBooks.startpage <= serialNumber.id AND invoiceBooks.endPage >= serialNumber.id
+                -- LEFT JOIN invoiceBooks ON (invoiceBooks.startPage = serialNumber.id OR invoiceBooks.startpage <= serialNumber.id) AND (invoiceBooks.endPage = serialNumber.id OR invoiceBooks.endPage >= serialNumber.id)
                 LEFT JOIN bookSerials ON bookSerials.invoiceBookID = invoiceBooks.invoiceBookID AND bookSerials.serialNumber = serialNumber.id
                 LEFT JOIN `transaction` ON CONCAT_WS('-',SUBSTRING_INDEX(transactionTypeNumber,'-',2),TRIM(leading '0' from SUBSTRING_INDEX(transactionTypeNumber,'-',-1))) = CONCAT_WS('-', invoiceBooks.bookType, invoiceBooks.bookNumber, serialNumber.id) AND `transaction`.transactionTypeNumber LIKE '" . $params['bookType'] . "-%'
                 WHERE invoiceBooks.bookNumber IS NOT NULL AND `transaction`.transactionID IS NULL AND bookSerials.bookSerialID IS NULL
